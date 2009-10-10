5e69ffe12343db07532907d5e4e54bbb4b2f9968
bonnier.vincent <bonnier.vincent@46b7009c-b4c1-11de-997e-c5eb2a8ae348>
2009-10-10 20:01:18 +0000

    Mise en place des premieres classes, modele, vue et controleur sur lesquels se greffera le travail de chacun. Ainsi sera mis en place au plus vite une interface graphique pour pouvoir tester les premiers algorithme. On fera l'import des differentes librairies de gestions de graphes. etc.

---
+++ trunk/graphViewer/src/graphviewer/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package graphviewer;
+
+/**
+ *
+ * @author vincent
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Model graphmodel ;
+        View graphview ;
+
+        Controler graphcontrol = new Controler(graphmodel, graphview);
+        graphview = new View(graphcontrol);
+
+    }
+
+}
