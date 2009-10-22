def3356a3186d500863057be9b5e1b55a652a985
monchrys <monchrys@46b7009c-b4c1-11de-997e-c5eb2a8ae348>
2009-10-22 16:45:44 +0000

    Implementation du graph de visualisation. Construction complete du graph de visualisation lors de l'initialisation seulement ! ! ! Attention au ajoute de noeuds en cours de test pour réoganisation.

--- graphViewer/src/graphviewer/model/visualisation/GraphDeVisualisation.java
+++ graphViewer/src/graphviewer/model/visualisation/GraphDeVisualisation.java
@@ -18,47 +18,35 @@ import graphviewer.vue.VueNoeud;
 public class GraphDeVisualisation{
 
     
-        Graph<VueNoeud,VueArrete> graph = new UndirectedSparseGraph();;
+    Graph<VueNoeud,VueArrete> graph = new UndirectedSparseGraph();;
 
     public GraphDeVisualisation(Graph newGraph) {
-        //TODO pour chaque noeud on cree un objet VueNoeud
-
+    
         VueNoeud vn = null;
+        VueArrete va = null;
+        ModeleNoeud n1=null, n2=null;
+        
         for (Object object : newGraph.getVertices()) {
             vn = new VueNoeud((ModeleNoeud)object);
             graph.addVertex(vn);
-            //ajoute dans le graphe
         }
-
-
-        VueArrete va = null;
-        int nbredge  = newGraph.getEdgeCount();
-
-
-        
-
-        //TODO pour chaque arrete on cree un objet VueArrete relier correctement !
-
-     /*
-        for (Object n1 : newGraph.getVertices()) {
-            for (Object n2 : newGraph.getVertices()) {
-                
-                if ((newGraph.findEdge((ModeleNoeud)n1,(ModeleNoeud) n2)) != null){
-                    va = new VueArrete(
-                            (ModeleArrete)(newGraph.findEdge((ModeleNoeud)n1,(ModeleNoeud) n2)),
-                            getVueNoeudByID(((ModeleNoeud)n1).getID()),
-                            getVueNoeudByID(((ModeleNoeud)n2).getID())
-                            );
-                    graph.addEdge(va, vn, vn, arg3);
+    
+        for (Object o1 : newGraph.getVertices()) {
+            n1 = (ModeleNoeud)o1;
+            for (Object o2 : newGraph.getVertices()) {
+                n2 = (ModeleNoeud)o2;
+    
+                if ((newGraph.findEdge(n1, n2)) != null){
+                    if ( graph.findEdge(
+                            getVueNoeudByID(n1.getID()),
+                            getVueNoeudByID(n2.getID())
+                            ) == null){
+                        va = new VueArrete((ModeleArrete)(newGraph.findEdge(n1,n2)));
+                        graph.addEdge(va, getVueNoeudByID(n1.getID()), getVueNoeudByID(n2.getID()));
+                    }
                 }
-                va = new VueArrete((ModeleArrete)object);
-
             }
         }
-
-*/
-        // AFFICHAGE DU GRAPH DE VISUALISATION
-        System.out.println(graph);
     }
 
     public Graph<VueNoeud, VueArrete> getGraph() {
@@ -66,38 +54,22 @@ public class GraphDeVisualisation{
     }
 
 
-
-
-
-/*
-
-    public VueNoeud getVueDuNoeud(ModeleNoeud){
-        return
-    }
-*/
-
-/*
-    
-    public void addArret(ModeleArrete art,ModeleNoeud n1 , ModeleNoeud n2 ) {
-       // TODO faire le noeud de visualisation
-        VueArrete newNoeud = new VueArrete(art);
-
-
-        
-        graph.addEdge(newNoeud, graph.getvn1, n2);
+    public VueNoeud getVueNoeudByID(int id){
+        VueNoeud vn = null;
+        for (Object object : graph.getVertices()) {
+            vn = (VueNoeud)object;
+            if (vn.getId() == id)
+                break;
+        }
+        return vn;
     }
 
-    public void addNoeud(ModeleNoeud n1) {
-
-        // TODO faire le noeud de visualisation
-        VueNoeud newNoeud = new VueNoeud(n1);
-
-
-
-        
-        graph.addVertex(newNoeud);
+    @Override
+    public String toString() {
+        return graph.toString();
     }
-*/
+
+
 
         // TODO methode de manipulatioon de l ensemble du graph.
 
